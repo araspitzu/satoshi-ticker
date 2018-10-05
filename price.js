df2265286a7184bafe41116a249e9831895d9ea2
araspitzu <a.raspitzu@gmail.com>
2018-10-05 16:03:24 +0200

    Use toLocaleString() to group digits

--- price.js
+++ price.js
@@ -14,18 +14,17 @@ function refreshPrice() {
         var result = JSON.parse(data);
         eurbtcPrice = result.bpi.EUR.rate_float;
         satoshiEUR = (COIN_SATOSHI / eurbtcPrice).toFixed(0);
+
         $('#satfiat').empty();   
         $('#satfiat-10').empty();
         $('#satfiat-100').empty();
         $('#satfiat-1000').empty();
-
         
-        $('#satfiat').append("1€ = " + satoshiEUR + " sats"); 
-        $('#satfiat-10').append("10€ = " + satoshiEUR * 10 + " sats"); 
-        $('#satfiat-100').append("100€ = " + satoshiEUR * 100 + " sats"); 
-        $('#satfiat-1000').append("1000€ = " + satoshiEUR * 1000 + " sats"); 
+        $('#satfiat').append("1€ = " + (satoshiEUR).toLocaleString() + " sats"); 
+        $('#satfiat-10').append("10€ = " + (satoshiEUR * 10).toLocaleString() + " sats"); 
+        $('#satfiat-100').append("100€ = " + (satoshiEUR * 100).toLocaleString() + " sats"); 
+        $('#satfiat-1000').append("1000€ = " + (satoshiEUR * 1000).toLocaleString() + " sats"); 
 
     });
-
     
 }
