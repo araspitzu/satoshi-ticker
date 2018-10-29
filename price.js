16b03a01eb4e8018ef8147f3903486c4917cd1b8
verdu89 <verdu89@gmail.com>
2018-10-29 21:31:37 +0100

    added custom Fiat

--- price.js
+++ price.js
@@ -14,20 +14,31 @@ function refreshPrice() {
 
     $.get("https://api.coindesk.com/v1/bpi/currentprice.json", function(data){
         var result = JSON.parse(data);
-        fiatBTCPrice = result.bpi[selectedCurrency].rate_float;
-        satoshiFIAT = (COIN_SATOSHI / fiatBTCPrice).toFixed(0);
-        fiatSymbol = result.bpi[selectedCurrency].symbol;
+        var  fiatBTCPrice = result.bpi[selectedCurrency].rate_float;
+        var satoshiFIAT = (COIN_SATOSHI / fiatBTCPrice).toFixed(0);
+        var fiatSymbol = result.bpi[selectedCurrency].symbol;
+        var customFiat = $('#satfiat-custom').val();
+        var customSat = (customFiat * satoshiFIAT);
+        var risultato = " = " + customSat.toLocaleString() + " sats";
 
+
+        
         $('#satfiat').empty();   
         $('#satfiat-10').empty();
         $('#satfiat-100').empty();
         $('#satfiat-1000').empty();
+        $('#customfiat').empty();
+        
+
         
         $('#satfiat').append("1 "+ fiatSymbol +" = " + (satoshiFIAT).toLocaleString() + " sats"); 
         $('#satfiat-10').append("10 "+ fiatSymbol +" = " + (satoshiFIAT * 10).toLocaleString() + " sats"); 
         $('#satfiat-100').append("100 "+ fiatSymbol +" = " + (satoshiFIAT * 100).toLocaleString() + " sats"); 
         $('#satfiat-1000').append("1000 "+ fiatSymbol +" = " + (satoshiFIAT * 1000).toLocaleString() + " sats"); 
-
+        $('#customfiat').append(fiatSymbol);
+        $('#customfiat').append(risultato);
     });
     
 }
+
+
