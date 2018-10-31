e083edda1a5a4f7b1c2dfc0d5bf08dd2cc500290
araspitzu <a.raspitzu@gmail.com>
2018-10-31 18:55:27 +0100

    Caching version

--- price.js
+++ price.js
@@ -1,36 +1,88 @@
 $(document).ready(function() {
 
-	refreshPrice();
+    printPrice();
+    localStorage[BPI_IDX] = null;
 
-	window.setInterval(refreshPrice, 15000); //15s
+	window.setInterval(printPrice, CHECK_INTERVAL_MILLIS);
 });
 
 var COIN_SATOSHI = 100000000; //Number of satoshis in 1BTC
+var CHECK_INTERVAL_MILLIS = 60000 //60s
+var BPI_IDX = "BPI";
+var BPI_URL = "https://api.coindesk.com/v1/bpi/currentprice.json";
 
-function refreshPrice() {
-    console.log('Refreshing the price');
+function printPrice() {
 
     var selectedCurrency = $("#currency-selector").find(":selected").val();
-    var customFiatValue = $('#satfiat-custom').val();
 
-    $.get("https://api.coindesk.com/v1/bpi/currentprice.json", function(data){
-        var result = JSON.parse(data);
-        var fiatBTCPrice = result.bpi[selectedCurrency].rate_float;
+    getCoindeskBPI(function(bpi) {
+
+        var fiatBTCPrice = bpi[selectedCurrency].rate_float;
         var satoshiFIAT = (COIN_SATOSHI / fiatBTCPrice).toFixed(0);
-        var fiatSymbol = result.bpi[selectedCurrency].symbol;
-        var customSatValue = (customFiatValue * satoshiFIAT);
+        var fiatSymbol = bpi[selectedCurrency].symbol;
         
         $('#satfiat').empty();   
         $('#satfiat-10').empty();
         $('#satfiat-100').empty();
         $('#satfiat-1000').empty();
-        $('#customfiat').empty();
             
         $('#satfiat').append("1 "+ fiatSymbol +" = " + (satoshiFIAT).toLocaleString() + " sats"); 
         $('#satfiat-10').append("10 "+ fiatSymbol +" = " + (satoshiFIAT * 10).toLocaleString() + " sats"); 
         $('#satfiat-100').append("100 "+ fiatSymbol +" = " + (satoshiFIAT * 100).toLocaleString() + " sats"); 
         $('#satfiat-1000').append("1000 "+ fiatSymbol +" = " + (satoshiFIAT * 1000).toLocaleString() + " sats"); 
-        $('#customfiat').append(fiatSymbol + " = " + customSatValue.toLocaleString() + " sats");
+   
     });
     
 }
+
+function updateInput() {
+    var customFiatValue = $('#satfiat-custom').val();
+    var selectedCurrency = $("#currency-selector").find(":selected").val();
+
+    getCoindeskBPI(function(bpi) {
+
+        var fiatBTCPrice = bpi[selectedCurrency].rate_float;
+        var satoshiFIAT = (COIN_SATOSHI / fiatBTCPrice).toFixed(0);
+        var fiatSymbol = bpi[selectedCurrency].symbol;
+        var customSatValue = (customFiatValue * satoshiFIAT);
+
+        $('#customfiat').empty();
+        $('#customfiat').append(fiatSymbol + " = " + customSatValue.toLocaleString() + " sats");
+
+    });
+
+}
+
+
+function getCoindeskBPI(onDataAvailable) {
+
+    var coinDeskResponse = localStorage[BPI_IDX];
+
+    if(coinDeskResponse === undefined) {
+        console.log("Cached response was empty");
+        $.get(BPI_URL, function(data){ 
+            localStorage[BPI_IDX] = data;
+            onDataAvailable(JSON.parse(localStorage[BPI_IDX]).bpi); 
+        });
+    } else {
+
+        var responseDate = new Date(JSON.parse(coinDeskResponse).time.updatedISO); //1min granularity
+        var millisElapsed = Math.floor((Date.now() - responseDate));
+
+        if(millisElapsed < CHECK_INTERVAL_MILLIS) {
+            console.log("Using cached response:");
+            onDataAvailable(JSON.parse(coinDeskResponse).bpi);
+        } else {
+            $.get(BPI_URL, function(data){ 
+                console.log("Refreshed the response");
+                localStorage[BPI_IDX] = null; //??
+                localStorage[BPI_IDX] = data;
+                onDataAvailable(JSON.parse(localStorage[BPI_IDX]).bpi); 
+            });
+        }
+    }
+
+
+}
+
+
